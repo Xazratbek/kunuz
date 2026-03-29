0a89f2c409cff74fd083aba91438447790d610fd
Khazratbek <xazratbek123@gmail.com>
2026-03-29 16:23:34 +0500

    initial commit

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#g)ih!-9c!6uif_m-d^%fl%0ig+0w$2&$!iiadq^t^&_#@%d_t'
+SECRET_KEY = 'django-insecure-%owe3pvpkti=ms&rd2zhuqme)dgid3-rn4$(jsas967y%@a_1k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'news',
 ]
 
 MIDDLEWARE = [
--- news/models.py
+++ news/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
-
-class Article(models.Model):
-    title = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True, blank=True)
-    content = models.TextField()
+# Create your models here.
